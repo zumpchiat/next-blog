18ab28fff47b155525177102b7606414b57cb819
zumpchiat <zumpchiat@gmail.com>
2024-10-21 21:08:03 -0300

    category delete

--- app/api/(dashboard)/categories/[category]/route.ts
+++ app/api/(dashboard)/categories/[category]/route.ts
@@ -1,6 +1,7 @@
 import connect from "@/lib/db";
 import Category from "@/lib/modals/category";
 import User from "@/lib/modals/user";
+import { request } from "http";
 import { Types } from "mongoose";
 import { NextResponse } from "next/server";
 
@@ -60,6 +61,58 @@ export const PATCH = async (request: Request, context: { params: any }) => {
     );
   } catch (error: any) {
     return new NextResponse("Error in updating category" + error.message, {
+      status: 500,
+    });
+  }
+};
+
+export const DELETE = async (request: Request, context: { params: any }) => {
+  const categoryId = context.params.category;
+  try {
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get("userId");
+
+    if (!userId || !Types.ObjectId.isValid(userId)) {
+      return new NextResponse(
+        JSON.stringify({ message: "Invalid or missing userId" }),
+        { status: 400 }
+      );
+    }
+
+    if (!categoryId || !Types.ObjectId.isValid(categoryId)) {
+      return new NextResponse(
+        JSON.stringify({ message: "Invalid or missing categoryId" }),
+        { status: 400 }
+      );
+    }
+
+    await connect();
+    const user = await User.findById(userId);
+    if (!user) {
+      return new NextResponse(JSON.stringify({ message: "User not found" }), {
+        status: 404,
+      });
+    }
+
+    const category = await Category.findOne({ _id: categoryId, user: userId });
+
+    if (!category) {
+      return new NextResponse(
+        JSON.stringify({
+          message: "category not found or not belong to the user",
+        }),
+        { status: 404 }
+      );
+    }
+
+    await Category.findByIdAndDelete(categoryId);
+
+    return new NextResponse(
+      JSON.stringify({ message: "Category is deleted" }),
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return new NextResponse("Error in deleting category" + error.message, {
       status: 500,
     });
   }
